488aa3c132c201d0904d6121e735961d9781b3d7
sophiecogswell24@hotmail.com Cogswell <sophiecogswell24@hotmail.com>
2018-08-12 15:20:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,9 +4,3 @@ const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
  
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
-const currentUser = 'Edsger Dijkstra';
- 
- 
-const shortGreeting = ('shortGreeting') ;
- 
-shortGreeting;
