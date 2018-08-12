6280cf4ebdb9f237b44a08edc788cd53714e34b4
sophiecogswell24@hotmail.com Cogswell <sophiecogswell24@hotmail.com>
2018-08-12 15:25:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,5 +14,12 @@ const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 const shortGreeting = `Welcome, ${currentUser}`;
 
 
+ 'Edsger Dijkstra'.length;
+//=> 15
  
+'Edsger Dijkstra'[15];
+//=> undefined
+ 
+'Edsger Dijkstra'[14];
+//=> "a"
 
