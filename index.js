4b64c147d239acc265031213f1609cb72236d0c9
sophiecogswell24@hotmail.com Cogswell <sophiecogswell24@hotmail.com>
2018-08-12 15:21:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const currentUser = 'Grace Hopper';
  
-
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
  
-const shortGreeting = `Welcome, ${currentUser}`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
